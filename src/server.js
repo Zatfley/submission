fcf114869dcb820bc754e370afa3c6628f36b2bb
Zatfley <alfi.dim1612@gmail.com>
2022-10-24 13:08:24 +0700

    First Commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.ENV_NODE !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+  await server.start();
+  console.log(`Server Berjalan pada ${server.info.uri}`);
+};
+
+init();
