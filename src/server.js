049bc4df2beb7a5f872ac9f7abae9221fb6799c0
Zatfley <alfi.dim1612@gmail.com>
2023-04-03 21:42:17 +0700

    update host

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 9000,
-    host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
